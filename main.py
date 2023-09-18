4ad30afd4acffa791fb916cb62c960068e9d950f
D27N <Triznuk.N@mail.ru>
2023-09-18 15:09:59 +0300

    Fix: change file path from absolute to relative

--- main.py
+++ main.py
@@ -17,17 +17,20 @@ def telegram_bot(token):
         file_name = message.video.file_name
         file_info = bot.get_file(message.video.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        with open(f'C:\\Proga\\from_video_in_audio\\{file_name}', 'wb') as video_file:
+        with open(file_name, 'wb') as video_file:
             video_file.write(downloaded_file)
         video = mpe.VideoFileClip(file_name)
         my_audio = video.audio
         my_audio.write_audiofile('new_audio.mp3')
-        with open(r'C:\Proga\from_video_in_audio\new_audio.mp3', 'rb') as audio:
+        with open('new_audio.mp3', 'rb') as audio:
             bot.send_audio(message.chat.id, audio, title=f'Audiofile for {message.from_user.first_name}.mp3', reply_to_message_id=message.id)
         video.close()
-        os.remove(r'C:\Proga\from_video_in_audio\new_audio.mp3')
-        os.remove(f'C:\\Proga\\from_video_in_audio\\{file_name}')
+        os.remove('new_audio.mp3')
+        os.remove(file_name)
     bot.infinity_polling()
 
 if __name__ == '__main__':
     telegram_bot(token)
+
+
+
